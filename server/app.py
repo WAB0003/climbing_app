66a556711b1b236399a8f28a34a40c1079625198
Bill Brown <bill.a.brown90@gmail.com>
2023-05-16 10:42:56 -0700

    add session to app.py

--- server/app.py
+++ server/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, make_response, jsonify
+from flask import Flask, request, make_response, jsonify, session
 from flask_cors import CORS
 from flask_migrate import Migrate
 
